4052cbb8d0c4f1dd8d9f4445e33edd569f1872ad
Patrick Kyoyetera <pertriquek171@gmail.com>
2018-12-17 17:29:00 -0500

    Switching to boost/date_time time & date libraries

---
+++ src/utils/utils.hpp
@@ -0,0 +1,40 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+
+#include <unordered_map>
+#include <iostream>
+// #include "boost/date_time/gregorian/gregorian.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+
+using namespace boost;
+
+
+namespace utils {
+    typedef enum modifiers {
+        ENCasualTimeParser,
+        ENCasualDateParser,
+    } Modifiers;
+
+
+    typedef std::unordered_map<Modifiers, bool > Tags;
+
+    // ptime to date
+    // date to ptime
+    // struct tm tp ptime
+    // ptime to struct tm
+    // gregorian to struct tm
+    // struct tm to gregorian
+
+}
+
+
+
+
+/*
+struct tm ptimeToStructTM(posix_time::ptime _ptime) {
+
+}
+*/
+
+#endif
---
+++ test/test__ENCasualTimeParser.cpp
@@ -0,0 +1,84 @@
+
+#include <iostream>
+#include <chrono>
+#include "gtest/gtest.h"
+#include "src/parsers/en/ENCasualTimeParser.h"
+
+using namespace std;
+using std::atoi;
+
+
+class ENCasualTimeTest : public ::testing::Test {
+public:
+    ENCasualTimeParser ps;
+    parse::ParsedComponents _cp;
+    tm anchor;
+    string text1, text2, text3, text4, text5;
+    std::vector<parse::ParsedResult> results;
+
+    ENCasualTimeTest() {
+
+        text1 = "What about this morning!";
+        text2 = "This afternoon was unusually cold";
+        text3 = "the evening sunset is noice";
+        text4 = "How does noon today sound?";
+        text5 = "Eventually the night is calm";
+    }
+
+    ~ENCasualTimeTest() {}
+
+};
+
+
+TEST_F(ENCasualTimeTest, test1) {
+    posix_time::ptime t( posix_time::second_clock::local_time() );
+    anchor = posix_time::to_tm(t);
+
+    results = ps.execute(text1, t);
+
+    EXPECT_EQ(results[0].startDate.getYear(), anchor.tm_year);
+    EXPECT_EQ(results[0].startDate.getMonth(), anchor.tm_mon);
+    EXPECT_EQ(results[0].startDate.get_mDay(), anchor.tm_mday);
+    EXPECT_EQ(results[0].startDate.get_wDay(), anchor.tm_wday);
+    ASSERT_EQ(results[0].startDate.getHour(), 06);
+
+    results = ps.execute(text2, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 15);
+
+    results = ps.execute(text3, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 20);
+
+    results = ps.execute(text4, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 12);
+
+    results = ps.execute(text5, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 20);
+}
+
+
+TEST_F(ENCasualTimeTest, test2) {
+    string testRefDate("2018-02-13 18:16:27"); //, format("%Y-%m-%d %T");
+    posix_time::ptime t (posix_time::time_from_string(testRefDate));
+    anchor = posix_time::to_tm(t);
+
+    results = ps.execute(text1, t);
+
+    EXPECT_EQ(results[0].startDate.getYear(), anchor.tm_year);
+    EXPECT_EQ(results[0].startDate.getMonth(), anchor.tm_mon);
+    EXPECT_EQ(results[0].startDate.get_mDay(), anchor.tm_mday);
+    EXPECT_EQ(results[0].startDate.get_wDay(), anchor.tm_wday);
+    ASSERT_EQ(results[0].startDate.getHour(), 06);
+
+    results = ps.execute(text2, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 15);
+
+    results = ps.execute(text3, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 20);
+
+    results = ps.execute(text4, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 12);
+
+    results = ps.execute(text5, t);
+    ASSERT_EQ(results[0].startDate.getHour(), 20);
+
+}
