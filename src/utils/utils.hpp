9472ba76c48f6fb1d7414e89fc20f6e7a192f372
Patrick Kyoyetera <pertriquek171@gmail.com>
2018-12-26 15:06:10 -0500

    integer words

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -3,6 +3,7 @@
 
 
 #include <unordered_map>
+#include <map>
 #include <iostream>
 // #include "boost/date_time/gregorian/gregorian.hpp"
 #include "boost/date_time/posix_time/posix_time.hpp"
@@ -14,17 +15,35 @@ namespace utils {
     typedef enum modifiers {
         ENCasualTimeParser,
         ENCasualDateParser,
+        ENDeadlineFormatParser,
     } Modifiers;
 
 
     typedef std::unordered_map<Modifiers, bool > Tags;
 
-    // ptime to date
-    // date to ptime
-    // struct tm tp ptime
-    // ptime to struct tm
-    // gregorian to struct tm
-    // struct tm to gregorian
+
+//     ptime to date
+//     date to ptime
+//     struct tm tp ptime
+//     ptime to struct tm
+//     gregorian to struct tm
+//     struct tm to gregorian
+
+    typedef std::map<std::string, int> {
+        {"one",    1},
+        {"two",    2},
+        {"three",  3},
+        {"four",   4},
+        {"five",   5},
+        {"six",    6},
+        {"seven",  7},
+        {"eight",  8},
+        {"nine",   9},
+        {"ten",    10},
+        {"eleven", 11},
+        {"twelve", 12}
+    }INTEGER_WORDS;
+    // std::map<std::string, int> ;
 
 }
 
