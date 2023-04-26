b02709731f705a23f0efe2dfda004954b96c819f
patrick <patrick@forge.ai>
2023-04-26 14:44:28 -0400

    nodiscard

--- src/parsers/parsers.hpp
+++ src/parsers/parsers.hpp
@@ -8,26 +8,23 @@
 #include "src/utils/utils.hpp"
 
 namespace parser {
-    static const unsigned short FULL_MATCH = 0;
+static const unsigned short FULL_MATCH = 0;
 
-    class Parser {
-    protected:
-        bool strictMode;
-        std::regex pattern;
+class Parser {
+protected:
+    bool strictMode;
+    std::regex pattern;
 
-    public:
-        Parser();
+public:
+    //Parser(bool, std::regex);       // strictMode, pattern
+    [[nodiscard]]
+    virtual std::regex getPattern() const { return {}; };
 
-        //Parser(bool, std::regex);       // strictMode, pattern
-        virtual std::regex getPattern() const { return std::regex(); };
+    virtual parse::ParsedResult extract(std::string &, const std::smatch &, const posix_time::ptime &, long) {
+        parse::ParsedResult tmp{};
+        return tmp;
+    }
 
-        virtual parse::ParsedResult extract(std::string &, const std::smatch &, const posix_time::ptime &, long) {
-            parse::ParsedResult tmp{};
-            return tmp;
-        }
-
-        Result execute(std::string &, posix_time::ptime &);
-
-        ~Parser();
-    };
+    Result execute(std::string &, posix_time::ptime &);
+};
 }
